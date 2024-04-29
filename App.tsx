2030b501f2616782caa4971af236f15f82f9f5b2
˜vitororigens˜ <vitorabreufjb@hotmail.com>
2024-04-29 16:17:32 -0300

    commit inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  View } from 'react-native';
+import { Home } from './src/screen/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{
+      flex: 1
+    }}>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
