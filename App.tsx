841c88bca7c80ab4e444b2b9f101a2fa153ff8d1
vitororigens <vitorabreufjb@hotmail.com>
2024-04-30 11:13:48 -0300

    loader(adicao): adicao do component Loader.

--- App.tsx
+++ App.tsx
@@ -3,11 +3,19 @@ import { StatusBar } from 'expo-status-bar';
 //
 import theme from './src/theme';
 import { Start } from './src/screen/Start';
+import { useFonts } from 'expo-font';
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loader } from './src/components/Loader';
 
 export default function App() {
+  const [fontLoader] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
   return (
     <ThemeProvider theme={theme}>
-      <Start/>
+      {fontLoader ? <Start/> : <Loader/>}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
