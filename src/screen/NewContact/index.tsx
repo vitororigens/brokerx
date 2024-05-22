9edbf67212c8915bbfd90791b9a17f901593de7b
˜vitororigens˜ <vitorabreufjb@hotmail.com>
2024-05-22 13:02:44 -0300

    newcontact(adicao): tela novo contato

--- src/screen/NewContact/index.tsx
+++ src/screen/NewContact/index.tsx
@@ -1,16 +1,56 @@
+import { ScrollView, View } from "react-native";
 import { DefaultContainer } from "../../components/DefaultContainer";
-import { Container, StyledImage, Content } from "./styles";
+import { Container, StyledImage, Content, Input, Title, Button, TitleButton } from "./styles";
 import { MaterialIcons } from '@expo/vector-icons';
 
-export function NewContact(){
-    return(
+export function NewContact() {
+    return (
         <DefaultContainer title="Adicionar contato">
             <Container>
-            <Content>
-          <StyledImage >
-            <MaterialIcons name="add-a-photo" size={36} color="white" />
-          </StyledImage>
-        </Content>
+                <Content>
+                    <StyledImage >
+                        <MaterialIcons name="add-a-photo" size={36} color="white" />
+                    </StyledImage>
+                </Content>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <Title>
+                        Nome:
+                    </Title>
+                    <Input />
+                    <Title>
+                        CPF:
+                    </Title>
+                    <Input />
+                    <Title>
+                        Telefone:
+                    </Title>
+                    <Input />
+                    <Title>
+                        E-mail:
+                    </Title>
+                    <Input />
+                    <Title>
+                        Endereço:
+                    </Title>
+                    <Input />
+                    <Title>
+                        Observações:
+                    </Title>
+                    <Input />
+                    <View
+                        style={{
+                            width: '100%',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <Button>
+                            <TitleButton>
+                                Salvar
+                            </TitleButton>
+                        </Button>
+                    </View>
+                </ScrollView>
             </Container>
         </DefaultContainer>
     )
--- src/screen/NewContact/styles.ts
+++ src/screen/NewContact/styles.ts
@@ -1,7 +1,9 @@
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     width: 100%;
+    height: 90%;
     background: ${({theme}) => theme.COLORS.WHITE};
     position: relative;
     z-index: 11;
@@ -9,13 +11,14 @@ export const Container = styled.View`
     padding: 20px;
     margin-bottom: 20px;
     justify-content: center;
-    align-items: center;
+    
 `;
 
 export const Content = styled.View`
     flex-direction: row;
     width: 100%;
     justify-content: flex-end;
+    margin-bottom: 80px;
 `;
 
 export const StyledImage = styled.View`
@@ -29,4 +32,40 @@ export const StyledImage = styled.View`
     top: -40px;
     border-radius: 100px;
     z-index: 11;
+`;
+
+export const Input = styled.TextInput`
+    max-height: 40px;
+    min-height: 40px;
+    width: 100%;
+    border-radius: 8px;
+    background-color: ${({theme}) => theme.COLORS.GRAY_400};
+    margin-bottom: 10px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${({theme}) => theme.FONTE_SIZE.GG}px;
+    color: ${({theme}) => theme.COLORS.BLUE_800};
+    font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
+    
+    margin-bottom: 5px;
+`;
+
+export const Button = styled(TouchableOpacity)`
+    min-width: 100px;
+    max-width: 100px;
+    min-height: 40px;
+    max-height: 40px;
+    background-color: ${({theme}) => theme.COLORS.BLUE_800};
+    border-radius: 8px;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+`;
+
+export const TitleButton = styled.Text`
+    font-size: ${({theme}) => theme.FONTE_SIZE.GG}px;
+    color: ${({theme}) => theme.COLORS.WHITE};
+    font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
+    
 `;
