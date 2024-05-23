414bbf88e7ea2a5ec5702f808664efdb884310aa
˜vitororigens˜ <vitorabreufjb@hotmail.com>
2024-05-22 22:33:35 -0300

    immobie(adicao) parte informacao

--- src/screen/Immobile/index.tsx
+++ src/screen/Immobile/index.tsx
@@ -1,8 +1,15 @@
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { DefaultContainer } from '../../components/DefaultContainer';
 import { Button, Container, Divaider, Icon, Input, StyledImage, SubTitle, Title, TitleButton } from './styles';
+import { useState } from 'react';
 
-export function Immobile() {
+type ImmobileProps = {
+  showPicker: boolean;
+}
+
+export function Immobile({ showPicker }: ImmobileProps) {
+  const [selectedCategory, setSelectedCategory] = useState('');
   return (
     <DefaultContainer title='Adicionar Imóvel'>
       <Container>
@@ -15,16 +22,16 @@ export function Immobile() {
             width: '100%',
             justifyContent: 'center'
           }}>
-          <Button type='PRIMARY'>
-            <TitleButton>
-              Adicionar 
-            </TitleButton>
-          </Button>
-          <Button type='SECONDARY'>
-            <TitleButton>
-              Excluir
-            </TitleButton>
-          </Button>
+            <Button type='PRIMARY'>
+              <TitleButton>
+                Adicionar
+              </TitleButton>
+            </Button>
+            <Button type='SECONDARY'>
+              <TitleButton>
+                Excluir
+              </TitleButton>
+            </Button>
           </View>
           <Title>
             Localização
@@ -35,7 +42,7 @@ export function Immobile() {
           <Input />
           <View style={{
             flexDirection: 'row',
-            
+
           }}>
             <View
               style={{
@@ -49,9 +56,9 @@ export function Immobile() {
               <Input />
             </View>
             <View style={{
-                width: '65%'
-              }}
-              >
+              width: '65%'
+            }}
+            >
               <SubTitle>
                 Cidade:
               </SubTitle>
@@ -60,7 +67,7 @@ export function Immobile() {
           </View>
           <View style={{
             flexDirection: 'row',
-            
+
           }}>
             <View
               style={{
@@ -74,9 +81,9 @@ export function Immobile() {
               <Input />
             </View>
             <View style={{
-                width: '65%'
-              }}
-              >
+              width: '65%'
+            }}
+            >
               <SubTitle>
                 Estado:
               </SubTitle>
@@ -85,6 +92,132 @@ export function Immobile() {
           </View>
 
           <Divaider />
+
+          <Title>
+            Informações
+          </Title>
+
+          <SubTitle>
+            Tipo de imóvel
+          </SubTitle>
+          <Input placeholder='Apartamento'/>
+
+          {showPicker &&
+            <Picker
+              selectedValue={selectedCategory}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedCategory(itemValue)
+              }>
+              <Picker.Item label="Apartamento" value="Apartamento" />
+              <Picker.Item label="Casa" value="Casa" />
+            </Picker>
+          }
+              <View style={{
+            flexDirection: 'row',
+
+          }}>
+            <View
+              style={{
+                width: '33%',
+                marginRight: 15
+              }}
+            >
+              <SubTitle>
+              Área Const.:
+              </SubTitle>
+              <Input />
+            </View>
+            <View style={{
+              width: '28%',
+              marginRight: 15,
+            }}
+            >
+              <SubTitle>
+                Área total:
+              </SubTitle>
+              <Input />
+            </View>
+            <View style={{
+              width: '28%'
+            }}
+            >
+              <SubTitle>
+                Matrícula:
+              </SubTitle>
+              <Input />
+            </View>
+          </View>
+          <View style={{
+            flexDirection: 'row',
+
+          }}>
+            <View
+              style={{
+                width: '33%',
+                marginRight: 15
+              }}
+            >
+              <SubTitle>
+                N Banheiros:
+              </SubTitle>
+              <Input />
+            </View>
+            <View style={{
+              width: '28%',
+              marginRight: 15,
+            }}
+            >
+              <SubTitle>
+                N Quartos:
+              </SubTitle>
+              <Input />
+            </View>
+            <View style={{
+              width: '28%'
+            }}
+            >
+              <SubTitle>
+                N Suítes:
+              </SubTitle>
+              <Input />
+            </View>
+          </View>
+          <View style={{
+            flexDirection: 'row',
+
+          }}>
+            <View
+              style={{
+                width: '33%',
+                marginRight: 15
+              }}
+            >
+              <SubTitle>
+                Pos. Sol:
+              </SubTitle>
+              <Input />
+            </View>
+            <View style={{
+              width: '28%',
+              marginRight: 15,
+            }}
+            >
+              <SubTitle>
+                N Salas:
+              </SubTitle>
+              <Input />
+            </View>
+            <View style={{
+              width: '28%'
+            }}
+            >
+              <SubTitle>
+                N Vagas:
+              </SubTitle>
+              <Input />
+            </View>
+          </View>
+
 
         </ScrollView>
       </Container>
