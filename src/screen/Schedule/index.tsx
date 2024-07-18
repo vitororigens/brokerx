f8f0866e51c397c7c86e8bed9a10aacef8d1ed22
devjpedro <joaopedross.ti9@gmail.com>
2024-07-18 20:55:09 -0300

    skeletons schedule

--- src/screen/Schedule/index.tsx
+++ src/screen/Schedule/index.tsx
@@ -1,69 +1,85 @@
-import { FlatList, Text, View } from 'react-native';
-import { DefaultContainer } from '../../components/DefaultContainer';
-import { Button, Container, Content, Icon, Title } from './styles';
-import { ItemsContacts } from '../../components/ItemsContacts';
 import { useNavigation } from "@react-navigation/native";
-import useFirestoreCollection from '../../hooks/useFirestoreCollection';
-import { useUserAuth } from '../../hooks/useUserAuth';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { Rect } from "react-content-loader/native";
+import { FlatList } from "react-native";
+import { DefaultContainer } from "../../components/DefaultContainer";
+import { ItemsContacts } from "../../components/ItemsContacts";
+import useFirestoreCollection from "../../hooks/useFirestoreCollection";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import {
+  Button,
+  Container,
+  Content,
+  ContentSkeleton,
+  Icon,
+  Title,
+} from "./styles";
 
 export function Schedule() {
   const user = useUserAuth();
   const uid = user?.uid;
-  const data = useFirestoreCollection('Contacts');
+  const data = useFirestoreCollection("Contacts");
   const navigation = useNavigation();
   const [selectedItemId, setSelectedItemId] = useState<string | null>(null);
 
+  const isLoading = !!data;
+
   function handleNewContact() {
-    navigation.navigate('newcontact', { selectedItemId: undefined });
+    navigation.navigate("newcontact", { selectedItemId: undefined });
   }
 
   function handleEditItem(documentId: string) {
-    navigation.navigate('newcontact', { selectedItemId: documentId });
+    navigation.navigate("newcontact", { selectedItemId: documentId });
   }
 
   function handleCardItem(documentId: string) {
-    navigation.navigate('cardcontact', { selectedItemId: documentId });
+    navigation.navigate("cardcontact", { selectedItemId: documentId });
   }
 
   useEffect(() => {
     if (selectedItemId) {
-      navigation.navigate('newcontact', { selectedItemId });
+      navigation.navigate("newcontact", { selectedItemId });
     }
   }, [selectedItemId]);
 
   return (
-    <DefaultContainer showButtonGears title='Contatos'>
+    <DefaultContainer showButtonGears title="Contatos">
       <Container>
-        <Content>
-          <FlatList
-            data={data.filter((item) => item.uid === uid)}
-            renderItem={({ item }) => (
-              <ItemsContacts
-                id={item.id}
-                numero={item.phone}
-                title={item.name}
-                investor={item.investor}
-                resident={item.resident}
-                image={item.imageUrl}
-                showButton
-                onEdit={() => handleEditItem(item.id)}
-                onCard={() => handleCardItem(item.id)}
-              />
-            )}
-            keyExtractor={(item) => item.id}
-            ListEmptyComponent={
-              <Title>
-                você ainda não tem contatos lançados,
-                comece adicionando um contato
-              </Title>
-            }
-          />
+        {!isLoading ? (
+          <ContentSkeleton backgroundColor="#f5f5f5" foregroundColor="#e0e0e0">
+            <Rect width={"100%"} height={170} rx={20} ry={20} />
+          </ContentSkeleton>
+        ) : (
+          <Content>
+            <FlatList
+              data={data.filter((item) => item.uid === uid)}
+              renderItem={({ item }) => (
+                <ItemsContacts
+                  id={item.id}
+                  numero={item.phone}
+                  title={item.name}
+                  investor={item.investor}
+                  resident={item.resident}
+                  image={item.imageUrl}
+                  showButton
+                  onEdit={() => handleEditItem(item.id)}
+                  onCard={() => handleCardItem(item.id)}
+                />
+              )}
+              keyExtractor={(item) => item.id}
+              ListEmptyComponent={
+                <Title>
+                  você ainda não tem contatos lançados, comece adicionando um
+                  contato
+                </Title>
+              }
+            />
 
-          <Button onPress={handleNewContact}>
-            <Icon name='plus' />
-          </Button>
-        </Content>
+            <Button onPress={handleNewContact}>
+              <Icon name="plus" />
+            </Button>
+          </Content>
+        )}
       </Container>
     </DefaultContainer>
   );
--- src/screen/Schedule/styles.ts
+++ src/screen/Schedule/styles.ts
@@ -1,42 +1,48 @@
-import styled from "styled-components/native";
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 } from "@expo/vector-icons";
+import ContentLoader from "react-content-loader/native";
 import { TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
-export const Container =styled.View`
-    flex: 1;
-    padding: 20px;
+export const Container = styled.View`
+  flex: 1;
+  padding: 20px;
 `;
 
 export const Content = styled.View`
-    width: 100%;
-    background: ${({theme}) => theme.COLORS.WHITE};
-    position: relative;
-    z-index: 11;
-    border-radius: 20px;
-    padding: 20px;
-    margin-bottom: 20px;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  background: ${({ theme }) => theme.COLORS.WHITE};
+  position: relative;
+  z-index: 11;
+  border-radius: 20px;
+  padding: 20px;
+  margin-bottom: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContentSkeleton = styled(ContentLoader)`
+  padding: 20px;
+  margin-bottom: 15px;
 `;
 
 export const Title = styled.Text`
-    font-size: ${({theme}) => theme.FONTE_SIZE.GG}px;
-    color: ${({theme}) => theme.COLORS.BLUE_800};
-    font-weight: bold;
-    text-align: center;
-    margin-bottom: 15px;
+  font-size: ${({ theme }) => theme.FONTE_SIZE.GG}px;
+  color: ${({ theme }) => theme.COLORS.BLUE_800};
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 15px;
 `;
 
 export const Button = styled(TouchableOpacity)`
-    width: 50px;
-    height: 50px;
-    background-color: ${({theme}) => theme.COLORS.BLUE_800};
-    border-radius: 50px;
-    align-items: center;
-    justify-content: center;
+  width: 50px;
+  height: 50px;
+  background-color: ${({ theme }) => theme.COLORS.BLUE_800};
+  border-radius: 50px;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Icon = styled(FontAwesome5).attrs(({theme}) => ({
-    size: 22,
-    color: theme.COLORS.WHITE,
+export const Icon = styled(FontAwesome5).attrs(({ theme }) => ({
+  size: 22,
+  color: theme.COLORS.WHITE,
 }))``;
